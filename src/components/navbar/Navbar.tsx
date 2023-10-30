fbf676232de96f1017e660021d9aa92e407df0c3
Luan-Patrik <luanpereira722@gmail.com>
2023-10-30 00:33:31 -0300

    feat: Navbar

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,36 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import { buttonVariants } from '../ui/button'
+import { auth } from '@/lib/auth'
+import UserMenu from './UserMenu'
+import { getServerSession } from 'next-auth'
+
+const Navbar = async () => {
+  const session = await auth()
+
+  return (
+    <header className="sticky inset-x-0 top-0 z-40 border-b bg-background py-2">
+      <nav className="container flex items-center justify-between">
+        <Link href="/">
+          <Image src="/assets/logo.jpg" alt="logo" width={50} height={50} />
+        </Link>
+        <ul>
+          <li>
+            {session?.user ? (
+              <UserMenu user={session.user} />
+            ) : (
+              <Link
+                className={buttonVariants({ variant: 'outline' })}
+                href="sign-in"
+              >
+                Entrar
+              </Link>
+            )}
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
---
+++ src/components/navbar/UserAvatar.tsx
@@ -0,0 +1,30 @@
+import { AvatarProps } from '@radix-ui/react-avatar'
+import { Avatar, AvatarFallback } from '../ui/avatar'
+import Image from 'next/image'
+import { Icons } from '../Icons'
+import { User } from 'next-auth'
+
+interface UserAvatarProps extends AvatarProps {
+  user: Pick<User, 'image'>
+}
+
+const UserAvatar = ({ user, ...props }: UserAvatarProps) => {
+  return (
+    <Avatar {...props}>
+      {user.image ? (
+        <div className="relative aspect-square h-full">
+          <Image
+            fill
+            src={user.image}
+            alt="Avatar"
+            referrerPolicy="no-referrer"
+          />
+        </div>
+      ) : (
+        <AvatarFallback></AvatarFallback>
+      )}
+    </Avatar>
+  )
+}
+
+export default UserAvatar
