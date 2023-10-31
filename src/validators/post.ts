f1154d270827f1a087f3cc03c08dc6b9bc3a6237
Luan-Patrik <luanpereira722@gmail.com>
2023-10-31 02:25:38 -0300

    feat: created new validator

--- src/validators/post.ts
+++ src/validators/post.ts
@@ -1,7 +1,8 @@
 import { z } from 'zod'
 
 export const PostValidator = z.object({
-  content: z.string().min(3, { message: '3 caracteres requiridos' })
+  content: z.string().min(3, { message: '3 caracteres requiridos' }),
+  isPrivate: z.boolean().default(false),
 })
 
 export type PostCreationRequest = z.infer<typeof PostValidator>
