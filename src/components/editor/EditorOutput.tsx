e771c37ad7dbb99af2a9d1476f6b1cca60d064c9
Luan-Patrik <luanpereira722@gmail.com>
2023-11-08 15:00:57 -0300

    feat: create placeholder and change logic Notfound

--- src/components/editor/EditorOutput.tsx
+++ src/components/editor/EditorOutput.tsx
@@ -2,13 +2,15 @@
 
 import { useGetUserPostSettings } from '@/hooks/use-get-user-post-settings'
 import Editor from './Editor'
+import { notFound } from 'next/navigation'
 
 const EditorOutput = () => {
   const { data, isLoading } = useGetUserPostSettings()
 
   if (isLoading) return 'Loading...'
+  if (!data) return notFound()
 
-  return <Editor content={data?.content || ''} isPrivate={data?.private} />
+  return <Editor content={data.content || ''} isPrivate={data.private} />
 }
 
 export default EditorOutput
