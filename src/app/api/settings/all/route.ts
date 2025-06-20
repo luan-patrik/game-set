6f4e1e66813acccb8b2621381a516d1e0da7aacb
luan-patrik <luanpereira722@gmail.com>
2025-06-20 18:43:44 -0300

    refactor: enhance logging in GET API route for better error tracking and debugging

--- src/app/api/settings/all/route.ts
+++ src/app/api/settings/all/route.ts
@@ -1,8 +1,8 @@
-import { auth } from '@/lib/auth'
 import prisma from '@/lib/db'
 import { NextResponse } from 'next/server'
 
 export async function GET() {
+  console.log('API Route: Attempting to connect to DB and fetch file settings.')
   try {
     const allPostSettings = await prisma.fileSettings.findMany({
       where: {
@@ -25,11 +25,12 @@ export async function GET() {
         createdAt: 'desc',
       },
     })
-
+    console.log('API Route: Successfully fetched file settings.')
     return new NextResponse(JSON.stringify(allPostSettings), {
       status: 200,
     })
   } catch (error) {
+    console.error('API Route Error: Failed to fetch posts.', error)
     return new NextResponse('There was an error receiving the posts.', {
       status: 500,
     })
